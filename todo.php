f772afccc7c8c346c278bb02f8a53d5f1308c92f
np549 <np549@njit.edu>
2017-11-25 23:13:24 -0500

    Active record Assignment

---
+++ todo.php
@@ -0,0 +1,21 @@
+<?php
+class todo extends model {
+    public $id;
+    public $owneremail;
+    public $ownerid;
+    public $createddate;
+    public $duedate;
+    public $message;
+    public $isdone;
+
+
+
+    protected static $modelName = 'todo';
+    public static function getTablename(){
+        $tableName='todos';
+        return $tableName;
+    }
+
+ 
+}
+?>
