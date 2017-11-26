b4ffb4541483feb48ad5f397964afd449bae2243
np549 <np549@njit.edu>
2017-11-26 16:46:00 -0500

    final code

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ define('DATABASE', 'np549');
 define('USERNAME', 'np549');
 define('PASSWORD', 'X7bBAA4xP');
 define('CONNECTION', 'sql2.njit.edu');
+
 /*Autoloader Class*/
 /*Autoloader that incorporates namespaces*/
 class Manage {
